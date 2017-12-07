1404e00d1f40d3c5741919c249c7cedfa744554c
Unknown <u1536411@uel.ac.uk>
2017-12-07 17:36:10 +0000

    Movement_Camera

---
+++ Team_Game_3/Assets/Scripts/Movement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Movement Variables ------------------------
+    [Header("Movement Variables")]
+    //Controls Player Speed
+    public float Mspeed = 5f;
+    //Stores Targets Position
+    public Vector3 targetPosition;
+    //If true move
+    private bool isMoving;
+
+    //Animator
+    private Animator PC_Animator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        targetPosition = transform.position;
+        isMoving = false;
+        PC_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if left click pressed settarget position to where was clicked
+        if (Input.GetMouseButton(0))
+            SetTargetPosition();
+        //if ismoving = true run moveplayer script
+        if (isMoving)
+            MovePlayer();
+
+        Animate();
+    }
+
+    void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            targetPosition = hit.point;
+            isMoving = true;
+        }
+
+    }
+
+    void MovePlayer()
+    {
+        Vector3 targetPosition2 = new Vector3(targetPosition.x,
+                                              this.transform.position.y,
+                                              targetPosition.z);
+        transform.LookAt(targetPosition2);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition2, Mspeed * Time.deltaTime);
+
+        if (transform.position == targetPosition2)
+        {
+            isMoving = false;
+        }
+    }
+
+    void Animate()
+    {
+        if (isMoving == true)
+        {
+            PC_Animator.SetBool("Walking", true);
+        }
+        else
+        {
+            PC_Animator.SetBool("Walking", false);
+        }
+    }
+
+}
---
+++ Team_Game_3/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    //Public variable to store a reference to the player game object
+    public GameObject player;   
+    //Private variable to store the offset distance between the player and camera
+    public Vector3 offset;
+    private float minOffset = 1.28f;
+    private float maxOffset = 16.28f;
+    public Transform target;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+
+    }
+
+    private void Update()
+    {
+            MouseWheelScroll();
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+
+    
+    }
+
+    void MouseWheelScroll()
+    {
+        if ((Input.GetAxis ("Mouse ScrollWheel") > 0) && offset.y > minOffset)
+        {
+                offset += new Vector3(0, -1, 1); 
+        }
+
+        if ((Input.GetAxis("Mouse ScrollWheel") < 0) && offset.y < maxOffset)
+        {
+                offset += new Vector3(0, 1, -1);
+        }
+
+    }
+}
